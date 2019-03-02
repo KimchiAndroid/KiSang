7f6c71bac40f6f76c67b7aacf3bc08950b4dbe50
wjp103 <wjp103@naver.com>
2019-03-02 15:42:20 +0900

    Merge pull request #1 from wjp103/master (#3) (#4) (#6) (#8)

--- app/src/main/java/com/example/kisang/SetAlarm.java
+++ app/src/main/java/com/example/kisang/SetAlarm.java
@@ -3,13 +3,11 @@ package com.example.kisang;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import android.R.*;
-
 public class SetAlarm extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.SetAlarm);
+        setContentView(R.layout.set_alarm);
     }
 }
