cea77b532ee02846941fa48f15025fb2cbc6e70e
hyunwoo <47684593+lji9605@users.noreply.github.com>
2019-03-02 15:11:47 +0900

    Merge pull request #1 from wjp103/master (#3) (#4) (#6)

--- app/src/main/java/com/example/kisang/MainActivity.java
+++ app/src/main/java/com/example/kisang/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.kisang;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+
+import android.R.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        List<String> list = new ArrayList<>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, list);
+
+        listview.setAdapter(adapter);
     }
 }
+
+
