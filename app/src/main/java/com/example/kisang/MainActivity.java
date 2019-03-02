ecb87aa607ac3d1fec8928e5b987ef0d0314f937
wjp103 <wjp103@naver.com>
2019-03-02 15:42:20 +0900

    Merge pull request #1 from wjp103/master (#3) (#4) (#6) (#8)

--- app/src/main/java/com/example/kisang/MainActivity.java
+++ app/src/main/java/com/example/kisang/MainActivity.java
@@ -1,34 +1,68 @@
 package com.example.kisang;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
-
-import android.R.*;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView listview;
+    ArrayList<Time> alarmTimeList;
+    ArrayList<String> alarmTimeStringList;
+    ArrayAdapter<String> adapter;
+    ListView listView;
+    Intent addIntent;
+
+    static final int REQ_ADD_ALARM = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        alarmTimeList = new ArrayList<>();
+        alarmTimeStringList = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alarmTimeStringList);
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+    }
+
+    public void onAddBtnClicked(View v) {
+        addIntent = new Intent(getApplicationContext(), SetAlarm.class);
+        startActivityForResult(addIntent, REQ_ADD_ALARM);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+//        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == REQ_ADD_ALARM) {
+            if (resultCode == RESULT_OK) {
+                addIntent = getIntent();
+                int hour = addIntent.getIntExtra("hour", -1);
+                int minute = addIntent.getIntExtra("minute", -1);
+
+                Log.v("MainActivity", hour+" : "+minute);
+                Time tmpTime = new Time(hour, minute);
+                alarmTimeList.add(tmpTime);
+                alarmTimeStringList.add(tmpTime.getHour()+" : "+tmpTime.getMinute());
+                adapter.notifyDataSetChanged();
+            }
+        }
+    }
 
 //        List<String> list = new ArrayList<>();
 //
-//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+//         = new ArrayAdapter<>(this,
 //                android.R.layout.simple_list_item_1, list);
 //
 //        listview.setAdapter(adapter);
-    }
 }
 
 
