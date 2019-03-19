66b845dda63c5e971f265c50d11bcff97c3c25d0
Sangyeon <arsene96@naver.com>
2019-03-19 15:32:45 +0900

    Merge pull request #1 from wjp103/master (#3) (#4) (#6) (#8) (#10)

--- app/src/main/java/com/example/kisang/SetAlarm.java
+++ app/src/main/java/com/example/kisang/SetAlarm.java
@@ -1,7 +1,11 @@
 package com.example.kisang;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TimePicker;
 
 public class SetAlarm extends AppCompatActivity {
 
@@ -10,4 +14,26 @@ public class SetAlarm extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.set_alarm);
     }
+
+    public void onSaveBtnClicked(View v) {
+        //시간 정보들을 저장한다.
+        TimePicker timePicker = (TimePicker)findViewById(R.id.timePicker); // initiate a time
+        timePicker.setIs24HourView(false);
+        int hour = timePicker.getCurrentHour();
+        int minute = timePicker.getCurrentMinute();
+        Log.v("SetAlarm", hour+" : "+minute);
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.putExtra("hour", hour);
+        intent.putExtra("minute", minute);
+
+        setResult(RESULT_OK, intent);
+        finish();
+        //activity_main.xml에 알람시간과 반복주기, switch on/off panel하나를 띄운다.
+    }
+
+    public void onCancelBtnClicked(View v) {
+        finish();
+    }
+
 }
